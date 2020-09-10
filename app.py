aebcca3a4499c7e7b63ee2bd9f5b6f4285869d8b
satish chary <satish.aeleshwara@gmail.com>
2020-09-10 18:43:43 +0530

    changes made

--- app.py
+++ app.py
@@ -1,35 +1,51 @@
 import os
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-#@app.route('/')
-#def hello_world():
-#    return 'Hello World!'
-
 @app.route('/')
-@app.route('/<string:fname>/')
-@app.route('/<string:fname>/<int:sln>/<int:eln>')
-def fileread(fname=None, sln=None, eln=None):
+def hello_world():
+    return 'Welcome to Home page'
+
+
+@app.route('/serve_file')
+def fileread():
+    fname = request.args.get('filename', type=str)
+    sln = request.args.get('startline', type=int)
+    eln = request.args.get('endline', type=int)
+
     if not fname:
         fname = 'file1.txt'
     filepath = os.path.join("Jrpythondev/",fname)
+    encode = "utf-8"
+    if fname == 'file4.txt' or fname == 'file2.txt':
+        encode = "utf-16"
+    
     try:
-        with open(filepath,'r',  encoding="cp437", errors='ignore') as fh:
+        with open(filepath,'r',  encoding=encode, errors='ignore') as fh:
             lines = fh.readlines()
+            linecount = len(lines)
             if (sln and eln):
-                lines = lines[sln:eln+1]
+                if (sln >= linecount or eln >= linecount):
+                    raise ValueError("Invalid arguments, start line number, end line number should be within line count") 
+                if(sln <= eln):
+                    lines = lines[sln:eln+1]
+                else:
+                   raise ValueError("Invalid arguments, start line number should be less than end line number")
     except Exception as e:
         response = exception_handler(exception=e, error=500)
         return response
 
-    return render_template('filecontent.html', lines=lines)
+    context = {"lines":lines, "filename":fname}
+    return render_template('filecontent.html', context=context)
 
 
 def exception_handler(exception, error):
-    message = "{}  --  {}".format(error, f"{repr(exception)}")
+    message = dict()
+    message['msg'] = "{}".format(f"{repr(exception)}")
+    message['error'] = "{}".format(error)
     return render_template('PageNotFound.html', message=message)
 
 if __name__ == '__main__':
